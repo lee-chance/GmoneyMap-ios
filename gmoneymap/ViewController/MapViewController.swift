366af1d3f06057f35a440361a8ffa878825f3b90
Changsu Lee <>
2021-06-02 01:10:58 +0900

    카카오맵 준비..

---
+++ gmoneymap/ViewController/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  gmoneymap
+//
+//  Created by Changsu Lee on 2021/06/02.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

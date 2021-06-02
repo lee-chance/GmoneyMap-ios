57c760c181c3f1979f614a9bf985c6f34ee64ef9
Changsu Lee <>
2021-06-02 10:43:34 +0900

    Kakao map 라이브러리 추가

---
+++ gmoneymap/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  gmoneymap
+//
+//  Created by Changsu Lee on 2021/06/02.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import<DaumMap/MTMapView.h>
+
+#endif /* Bridging_Header_h */
--- gmoneymap/ViewController/MapViewController.swift
+++ gmoneymap/ViewController/MapViewController.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MTMapViewDelegate {
+    
+    var mapView: MTMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView = MTMapView(frame: self.view.bounds)
+        if let mapView = mapView {
+            mapView.delegate = self
+            mapView.baseMapType = .standard
+            self.view.addSubview(mapView)
+        }
     }
 
 }
